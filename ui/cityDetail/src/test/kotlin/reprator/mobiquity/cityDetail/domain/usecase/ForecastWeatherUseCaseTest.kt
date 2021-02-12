feb4c4f485151ba1c39b2caf0c310c047eed8778
Vikram Singh <mr.vikram004@gmail.com>
2021-02-13 00:00:33 +0530

    ForecastWeatherRepositoryImplTest unit test complete

--- ui/cityDetail/src/test/kotlin/reprator/mobiquity/cityDetail/domain/usecase/ForecastWeatherUseCaseTest.kt
+++ ui/cityDetail/src/test/kotlin/reprator/mobiquity/cityDetail/domain/usecase/ForecastWeatherUseCaseTest.kt
@@ -1,5 +1,62 @@
 package reprator.mobiquity.cityDetail.domain.usecase
 
-import org.junit.Assert.*
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.google.common.truth.Truth
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.single
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import reprator.mobiquity.base.useCases.Success
+import reprator.mobiquity.cityDetail.TestFakeData.getFakeLocationModalDataList
+import reprator.mobiquity.cityDetail.domain.repository.ForecastWeatherRepository
+import reprator.mobiquity.cityDetail.modals.LocationRequestModal
+import reprator.mobiquity.testUtils.MainCoroutineRule
 
-class ForecastWeatherUseCaseTest
+@ExperimentalCoroutinesApi
+@RunWith(JUnit4::class)
+class ForecastWeatherUseCaseTest {
+
+    @Rule
+    @JvmField
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    @JvmField
+    @Rule
+    val coroutinesTestRule = MainCoroutineRule()
+
+    @MockK
+    lateinit var forecastWeatherRepository: ForecastWeatherRepository
+
+    lateinit var forecastWeatherUseCase: ForecastWeatherUseCase
+
+    private val input = LocationRequestModal("24.31", "76.32")
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this, true)
+
+        forecastWeatherUseCase = ForecastWeatherUseCase(forecastWeatherRepository)
+    }
+
+    @Test
+    fun `fetch forecast data from remote data source`() = coroutinesTestRule.runBlockingTest {
+        val output = getFakeLocationModalDataList()
+
+        coEvery {
+            forecastWeatherRepository.getForeCastWeatherRepository(any())
+        } returns flowOf(Success(output))
+
+        val result = forecastWeatherUseCase(input).single()
+
+        Truth.assertThat(result).isInstanceOf(Success::class.java)
+        Truth.assertThat((result as Success).data).isEqualTo(output)
+    }
+}
